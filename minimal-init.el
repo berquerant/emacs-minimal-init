f055433f94809cead2291940d547627728d16923
berquerant <sin@sin-pro.local>
2023-06-21 23:52:37 +0900

    Version 0.2.1

--- minimal-init.el
+++ minimal-init.el
@@ -3,7 +3,7 @@
 ;; Author: berquerant
 ;; Maintainer: berquerant
 ;; Created: 14 Jan 2023
-;; Version: 0.2.0
+;; Version: 0.2.1
 ;; Keywords: init
 ;; URL: https://github.com/berquerant/emacs-minimal-init-el
 
